47b982aadd886ad63ba80bbf7b30c19c066c6c6d
Fuad Hasan Shihab <85599261+fuadhasanshihab@users.noreply.github.com>
2024-08-14 12:14:42 +0600

    Update new.js

--- pwa/scripts/new.js
+++ pwa/scripts/new.js
@@ -42,9 +42,51 @@ if (window.matchMedia('(display-mode: standalone)').matches) {
 
         // Start Notifications
         document.querySelector('.notification-target-class').innerHTML += `
-            <div class="notification-content">
-                <p>This content is always appended to notifications.</p>
+
+            <div aria-hidden='true' aria-labelledby='modalFilterLabel' class='modal -left --fullScreen modal-collectibles fade' id='mdllCollectibles' tabindex='-1'>
+  <div class='modal-dialog modal-dialog-scrollable'>
+    <div class='center modal-content'>
+      <div class='modal-header' style='padding: 30px 0!important;'>
+        <div class=''>
+          <h1>Notification</h1>
+          <p>- no notification</p>
+        </div>
+        <button aria-label='Close' class='btn btnClose' data-bs-dismiss='modal' type='button'>
+        <i class='ri-close-fill'></i>
+        </button>
+      </div>
+      <div class='modal-body p-0'>
+        <div class='un-details-collectibles'>
+          <!-- body -->
+          <div class='bodyy centerrrrr'>
+            <img alt='no-notification-yiran-removebg-preview' border='0' src='https://i.ibb.co/bRFHgTj/no-notification-yiran-removebg-preview.png' style='width:: 100%!important;'/>
+          </div>
+        </div>
+      </div>
+      <div class='modal-footer d-block border-0 pt-2' style='padding: 20px 0!important;'>
+        <div class='centerrr content-modal-footer'>
+          <a class='btn btn-bid-items' href='https://fuadhasanshihab.wpush.io/subscribe'>
+            <p>
+              Push Notifications<!--span class='size-13'>(2.3)</span-->
+            </p>
+            <div class='ico'>
+              <i class='ri-notification-badge-line'></i>
             </div>
+          </a>
+          <a class='btn btn-bid-items' href='/p/subscribe.html'>
+            <p>
+              Email Subscription<!--span class='size-13'>(2.3)</span-->
+            </p>
+            <div class='ico'>
+              <i class='ri-mail-line'></i>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
         `;
         // End Notifications
 
